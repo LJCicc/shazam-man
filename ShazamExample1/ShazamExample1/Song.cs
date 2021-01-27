d60b87eaa3ea3e23c5b952f8145a70e0e577821e
LJCicc <57023579+LJCicc@users.noreply.github.com>
2021-01-26 19:49:18 -0500

    First checkin of initial objects
    Describing songs, releases, playlists, etc.

---
+++ ShazamExample1/ShazamExample1/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShazamExample1
+{
+    class Song
+    {
+        private string _songTitle;
+        private Artist Artist;
+        private float duration;
+
+        public string SongTitle { get => _songTitle; set => _songTitle = value; }
+        public Artist Artist1 { get => Artist; set => Artist = value; }
+        public float Duration { get => duration; set => duration = value; }
+    }
+}
